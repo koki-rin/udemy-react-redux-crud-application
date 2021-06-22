598efbd3307afec9e48ff4ed136151e8acc57ac1
koki-rin <duemakouki@yahoo.co.jp>
2021-06-23 00:33:19 +0900

    first

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 classNAme="App-title">hello , world</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
