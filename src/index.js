89f941395097b2a21a1b98ccb35959ab9457d2cf
koki-rin <duemakouki@yahoo.co.jp>
2021-06-26 17:22:39 +0900

    implement component with connect function.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+
+import { createStore } from 'redux'
+import App from './components/App';
 import './index.css';
 import reducer from './reducers'
-import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer)
@@ -20,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,13 +1,13 @@
-import { INCREMENT, DECREMENT } from "../actions";
+import { INCREMENT, DECREMENT } from "../actions"
 
 const initialState = { value: 0 }
 
 export default ( state= initialState, action ) => {
   switch (action.type) {
     case INCREMENT:
-      return { value: state.vlue + 1}
-    case DeCREMENT:
-      return { value: state.vlue - 1}
+      return { value: state.value + 1}
+    case DECREMENT:
+      return { value: state.value - 1}
     default:
      return state 
   }
